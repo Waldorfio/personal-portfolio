f117a3e7a7c70fbd894906c951efa20dcc4e7510
Walid VM <walid.hanifi707@gmail.com>
2022-11-21 13:40:57 +1000

    Add landing page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import './styles/style.css'
 
 function App() {
   return (
     <div>
+
+      <div id='landing-page'>
+        <h1>
+          Hello, I'm <span className='name'>Wally</span><br></br>
+          I'm a full stack web developer.
+        </h1>
+
+        <div id='view-work' className='name'>
+          View my work
+        </div>
+      </div>
+
     </div>
   );
 }
