6b6d4fb0ac1c1e1a513bd6096bdcaf7f37885b46
Walid VM <walid.hanifi707@gmail.com>
2022-11-23 16:26:05 +1000

    Fix typo in devicon label

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
                 <div><i className="devicon-jest-plain colored"></i><span>Jest</span></div>
                 <div><i className="devicon-git-plain colored"></i><span>Git</span></div>
                 <div><i className="devicon-webpack-plain colored"></i><span>Webpack</span></div>
-                <div><i className="devicon-npm-original-wordmark colored"></i><span>HTML</span></div>
+                <div><i className="devicon-npm-original-wordmark colored"></i><span>npm</span></div>
             </div>
         </div>
   
